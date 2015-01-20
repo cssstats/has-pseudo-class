fe10cdc7d11a9314a0057b3b0fa599ccf5458f2e
John Otander <johnotander@gmail.com>
2015-01-20 15:26:27 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function hasPseudoClass(options) {
+  options = options || {};
+
+  return true;
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var hasPseudoClass = require('..');
+
+describe('has-pseudo-class', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(hasPseudoClass(), true);
+  });
+});
